5822af270de768d17fb929d7fd810414584fc06f
nico-limo <nicolas.limonoff@gmail.com>
2021-04-04 19:44:50 -0300

    navbar responsive done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,226 @@
+import React from "react";
+//CSS
+import "../styles/Navbar.css";
+//Fonts
+import { faAddressCard,faBriefcase, faBookReader, faPaperPlane, faMoon, faAngleDoubleLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar">
+        <ul className="navbar__nav">
+            <li className="logo">
+                <a className="navbar__link" href="">
+                <FontAwesomeIcon id="navbar__logo" className="navbar__icon"  icon={faAngleDoubleLeft} />
+              <span className="navbar__text">Portafolio</span>
+                </a>
+            </li>
+          <l1 className="navbar__item">
+            <a className="navbar__link" href="#">
+            <FontAwesomeIcon className="navbar__icon"  icon={faAddressCard} />
+              <span className="navbar__text">Sobre Mi</span>
+            </a>
+          </l1>
+          <l1 className="navbar__item">
+            <a className="navbar__link" href="#">
+            <FontAwesomeIcon className="navbar__icon"  icon={faBookReader} />
+              <span className="navbar__text">Resumen</span>
+            </a>
+          </l1>
+          <l1 className="navbar__item">
+            <a className="navbar__link" href="#">
+            <FontAwesomeIcon className="navbar__icon"  icon={faBriefcase} />
+              <span className="navbar__text">Proyectos</span>
+            </a>
+          </l1>
+          <l1 className="navbar__item">
+            <a className="navbar__link" href="#">
+            <FontAwesomeIcon className="navbar__icon"  icon={faPaperPlane} />
+              <span className="navbar__text">Contacto</span>
+            </a>
+          </l1>
+          <l1 className="navbar__item">
+            <a className="navbar__link" href="#">
+            <FontAwesomeIcon className="navbar__icon"  icon={faMoon} />
+              <span className="navbar__text">Dark Mode</span>
+            </a>
+          </l1>
+        </ul>
+      </nav>
+
+      <main>
+        <h1>TITULO</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          consequuntur rerum quia dolor corporis iure recusandae consectetur aut
+          dicta ipsam id reiciendis exercitationem sint sunt, magnam illum esse
+          odio officia.
+        </p>
+      </main>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/container/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+//Components
+import Navbar from "../components/Navbar";
+import About from "../components/About";
+import Resume from "../components/Resume";
+import Proyects from "../components/Proyects";
+import Contact from "../components/Contact";
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    {/* <About/>
+    <Resume/>
+    <Proyects/>
+    <Contact/> */}
+    </>
+  );
+};
+
+export default App;
